559e0e30319c32a8b82dba8bc56eb79e390dcb30
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-18 07:44:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [Texas,California,Nebraska,Louisiana]
+  ["Texas","California","Nebraska","Louisiana"]
 end
 
 def add_element_to_end_of_array(array, element)
